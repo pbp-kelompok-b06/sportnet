a247a43717bae0b334be02a2072b08ed68cfd474
anjeelika <azzahraanjelikaborselano@gmail.com>
2025-10-23 04:22:00 +0700

    resolve conflict: bookmark model

--- Bookmark/models.py
+++ Bookmark/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from Event.models import Event
 
 class Bookmark(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
