808995f8fcdd650d737cd9bd9a4f5216e7a708f4
newjeasnz <jesiccatandra@gmail.com>
2025-10-22 11:01:42 +0700

    tes branch

--- sportnet/urls.py
+++ sportnet/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('organizer/', include('Organizer.urls')),
     path('profile/', include('Profile.urls')),
     path('review/', include('Reviews.urls')),
+    # ini komen
 ]
