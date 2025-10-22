0273b6158fed13dd48dd379ceed106c25c30c963
anjeelika <azzahraanjelikaborselano@gmail.com>
2025-10-23 04:19:23 +0700

    resolve conflict: bookmark model

--- Bookmark/models.py
+++ Bookmark/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
-# Create your models here.
-# class Bookmark(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     event = models.ForeignKey(Event, on_delete=models.CASCADE)
+class Bookmark(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
