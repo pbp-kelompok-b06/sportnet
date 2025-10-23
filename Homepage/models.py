1ffb48cc00c3ad696844e0c10795c0a7c46daf52
RayhanDerya <rayhanderya9@gmail.com>
2025-10-23 11:55:09 +0700

    Styling Homepage

--- Homepage/models.py
+++ Homepage/models.py
@@ -1,14 +1,10 @@
 from django.db import models
+from Event.models import Event
+
 
 # Create your models here.
 class CardEvent(models.Model):
-    name = models.CharField(max_length=255)
-    date = models.DateField()
-    location = models.CharField(max_length=255)
-    category = models.CharField(max_length=255)
-    description = models.TextField()
-    image = models.URLField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    parent_event = models.OneToOneField(Event, on_delete=models.CASCADE, null=True, blank=True)
     
     def __str__(self):
         return f"{self.name} - {self.date} - {self.location} - {self.price}"
