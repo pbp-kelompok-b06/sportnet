bd39ec852cc3c3c05a7dd4a591a8c3d209d443dd
RayhanDerya <rayhanderya9@gmail.com>
2025-10-23 17:34:01 +0700

    Routing Homepage/Navbar ke Profile dan Bookmark

--- sportnet/urls.py
+++ sportnet/urls.py
@@ -22,10 +22,10 @@ urlpatterns = [
     path('', include('Homepage.urls')),
     path('authenticate/', include('Authenticate.urls')),
     # path('booked-events/', include('BookedEvent.urls')),
-    # path('bookmark/', include('Bookmark.urls')),
+    path('bookmark/', include('Bookmark.urls')),
     path('event/', include("Event.urls")),
     # path('forum/', include("Forum.urls")),
     # path('notification/', include('Notification.urls')),
-    # path('profile/', include('Profile.urls')),
+    path('profile/', include('Profile.urls')),
     # path('review/', include('Reviews.urls')),
 ]
