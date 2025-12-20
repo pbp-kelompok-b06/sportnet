1c2cf67738b644ef2159a6e8ac78b5bd33516260
Muhammad Hafiz Hanan <muhammad.hafiz43@ui.ac.id>
2025-12-20 19:38:01 +0700

    Update urls.py

--- Forum/urls.py
+++ Forum/urls.py
@@ -7,9 +7,4 @@ urlpatterns = [
     path('<uuid:event_id>/', views.forum_page_view, name='forum_page'),
     path('edit/<int:post_id>/', views.edit_post_view, name='edit_post'),
     path('delete/<int:post_id>/', views.delete_post_view, name='delete_post'),
-    # === API UNTUK FLUTTER ===
-    path("api/add/<uuid:event_id>/", views.forum_api_add),
-    path("api/list/<uuid:event_id>/", views.forum_api_list),
-
-
 ]
