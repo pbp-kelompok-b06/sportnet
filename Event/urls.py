4aea452164886d50ce29dcd27d77c75d9f9db7a7
AnyaAleena <anya.aleenaa@gmail.com>
2025-12-21 17:14:26 +0700

    fix endpoint json join event

--- Event/urls.py
+++ Event/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("<uuid:event_id>/", views.event_detail, name="event_detail"),
     path('<uuid:event_id>/join/', views.join_event, name='join'),
     path("<uuid:event_id>/edit/", views.edit_event, name="edit_event"),
-    path('event/api/<uuid:event_id>/join/', views.join_event_json, name='join_event_json'),
+    path('api/<uuid:event_id>/join/', views.join_event_json, name='join_event_json'),
 
     path("json/", views.show_json, name="show_json"),
     path("json/<uuid:event_id>/", views.show_event_by_id_json, name="show_event_by_id_json"),
