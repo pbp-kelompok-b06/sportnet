6655f3505fd9ed6680e3a4db74571db85780bead
AnyaAleena <anya.aleenaa@gmail.com>
2025-12-21 22:46:56 +0700

    edit delete event flutter

--- Event/urls.py
+++ Event/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path('create-flutter/', views.create_event_flutter, name='create_event_flutter'),
     path("json/", views.show_json, name="show_json"),
     path("json/<uuid:event_id>/", views.show_event_by_id_json, name="show_event_by_id_json"),
+    path('delete-flutter/<uuid:event_id>/', views.delete_event_flutter, name='delete_event_flutter'),
+    path('edit-flutter/<uuid:event_id>/', views.edit_event_flutter, name='edit_event_flutter'),
 ]
